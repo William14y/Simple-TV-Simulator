346c92a5c555045545414f43bd191e2c7d037503
William14y <williamcavalcante06@gmail.com>
2025-02-28 10:47:08 -0300

    Final Commit

--- src/App.java
+++ src/App.java
@@ -3,49 +3,61 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        /* the method "menuTV" works by controlling the logic of the menu while the methods inside the
-           television class serve to store the properties that a television has, we could do this code more complex
-           by increasing more things inside the object class "television" but I want to make it simple for now */
-
-        menuTV();
-
-    }
-
-
-    public static void menuTV(){
-
         Television television = new Television();
-
-        System.out.println("\t--TV MENU--\t\n\n" +
-                "\t[1] Turn On\n" +
-                "\t[2] Turn Off\n" +
-                "\t[3] Increase Vol\n" +
-                "\t[4] Down Vol\n" +
-                "\t[5] Change Channel Up\n" +
-                "\t[6] Change Channel Down\n" +
-                "\t[0] Exit");
-
         Scanner menuOption = new Scanner(System.in);
-        int menuOpt = menuOption.nextInt();
+        int menuOpt;
 
-        switch (menuOpt) {
-            case 1:
-                television.setTurnOnOff(true);
-                System.out.println(television.isOnOrOff());
-                menuTV();
-                break;
-            case 2:
-                television.setTurnOnOff(false);
-                System.out.println(television.isOnOrOff());
-                menuTV();
-                break;
-            case 3:
-                System.out.println("How much volume do you want to increase?\n");
-                Scanner scannerVolUp = new Scanner(System.in);
-                int increasedVol = scannerVolUp.nextInt();
-                television.increaseVolume(increasedVol);
-                menuTV();
-                break;
-        }
+        do {
+            System.out.println("\t===TV MENU===\t\n\n" +
+
+                    "\tStatus: " + television.isOnOrOff() + "\n" +
+                    "\tActual Volume: " + television.getTotalVolume() + "\n" +
+                    "\tActual Channel: " + television.getChannel() + "\n\n" +
+
+                    "\t[1] Turn On\n" +
+                    "\t[2] Turn Off\n" +
+                    "\t[3] Increase Vol\n" +
+                    "\t[4] Down Vol\n" +
+                    "\t[5] Change Channel Up\n" +
+                    "\t[6] Change Channel Down\n" +
+                    "\t[0] Exit");
+
+            menuOpt = menuOption.nextInt();
+
+            switch (menuOpt) {
+                case 1:
+                    television.setTurnOnOff(true);
+                    System.out.println(television.isOnOrOff());
+                    break;
+                case 2:
+                    television.setTurnOnOff(false);
+                    System.out.println(television.isOnOrOff());
+                    break;
+                case 3:
+                    System.out.println("How much volume do you want to increase?\n");
+                    Scanner scannerVolUp = new Scanner(System.in);
+                    int increasedVol = scannerVolUp.nextInt();
+                    television.increaseVolume(increasedVol);
+                    break;
+                case 4:
+                    System.out.println("How much volume do you want to turn down?\n");
+                    Scanner scannerVolDown = new Scanner(System.in);
+                    int turnedDownVol = scannerVolDown.nextInt();
+                    television.downVolume(turnedDownVol);
+                    break;
+                case 5:
+                    television.channelUp();
+                    break;
+                case 6:
+                    television.channelDown();
+                    break;
+                case 0:
+                    System.out.println("Closing Program...");
+            }
+
+
+        } while (menuOpt != 0);
+        menuOption.close();
     }
+
 }
--- src/Television.java
+++ src/Television.java
@@ -4,6 +4,12 @@ public class Television {
     private int channel;
     private boolean turnOnOff;
 
+    public Television() {
+        this.totalVolume = 0;
+        this.channel = 0;
+        this.turnOnOff = turnOnOff;
+    }
+
     public void setTurnOnOff(boolean turnOnOff) {
         this.turnOnOff = turnOnOff;
     }
@@ -19,22 +25,42 @@ public class Television {
             return "The tv is off now";
     }
 
-    public void initialVol(){
-        this.totalVolume = 0;
-    }
-
     public void increaseVolume(int vol) {
-        this.totalVolume += vol;
-        System.out.println("Volume level turned to: " + getTotalVolume());
+        if (vol > 0) {
+            this.totalVolume += vol;
+            System.out.println("The volume turned to: " + this.totalVolume);
+        }
     }
 
     public void downVolume(int vol) {
-        this.totalVolume -= vol;
-        System.out.println("Volume level turned to: " + getTotalVolume());
+        if (vol > 0 && vol <= this.totalVolume) {
+            this.totalVolume -= vol;
+            System.out.println("The volume turned to:  " + this.totalVolume);
+        }
     }
 
     public int getTotalVolume() {
         return totalVolume;
     }
+
+
+    public void channelUp() {
+        if (this.channel <= 100) {
+            this.channel += 1;
+            System.out.println("Actual Channel: " + this.channel);
+        }
+    }
+
+    public void channelDown() {
+        if (this.channel >= 0) {
+            this.channel -= 1;
+            System.out.println("Actual Channel: " + this.channel);
+        }
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
 
 }
